c1e4c1c94cbb94ca203a7d29451189595f17425a
stefortezza <stefano.fortezza98@hotmail.it>
2024-07-31 15:11:11 +0200

    AGGIORNATI COMPONENT CON IL CSS E CONTROLLI.

--- src/app/section-home/section-home.component.ts
+++ src/app/section-home/section-home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SharedService } from '../service/shared-service.service';
 
 @Component({
   selector: 'app-section-home',
@@ -7,10 +8,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./section-home.component.scss']
 })
 export class SectionHomeComponent {
-  constructor(private router: Router) {}
+  showModal: boolean = false;
+  isAdmin: boolean = false;
+
+  constructor(private router: Router, private sharedService: SharedService) {
+    // Iscriviti ai cambiamenti di isAdmin
+    this.sharedService.isAdmin$.subscribe(isAdmin => {
+      this.isAdmin = isAdmin;
+    });
+  }
 
   navigateTo(route: string): void {
+    if (route === 'segreteria' && !this.isAdmin) {
+      this.showModal = true;
+      return;
+    }
     this.router.navigate([`/${route}`]);
     localStorage.setItem('isNavbarActive', 'true'); // Mantieni la navbar attiva
+  }
+
+  closeModal(): void {
+    this.showModal = false;
   }
 }
